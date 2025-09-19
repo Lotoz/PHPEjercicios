9826d40f28c77b5d0e55a4a96e078e5b773d8bef
Lotoz <144383551+Lotoz@users.noreply.github.com>
2025-09-19 15:59:52 +0200

    a

---
+++ relacion1/holamundo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relacion 1 - Hola mundo</title>
+    <link rel="shortcut icon" href="./playamar.png" type="image/x-icon">
+</head>
+<body>
+    <h1>Hola mundo en php</h1>
+    <h3 style='color:purple'>
+    <?php 
+        echo "Hola mundo"
+    ?>
+    </h3>
+</body>
+</html>
