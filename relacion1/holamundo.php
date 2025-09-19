542c0a36c9f1179b4d04bb6620bcdbd1dcc0d012
Lotoz <144383551+Lotoz@users.noreply.github.com>
2025-09-19 19:40:49 +0200

    a

--- relacion1/holamundo.php
+++ relacion1/holamundo.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relacion 1 - Hola mundo</title>
     <link rel="shortcut icon" href="./playamar.png" type="image/x-icon">
 </head>
+
 <body>
     <h1>Hola mundo en php</h1>
     <h3 style='color:purple'>
-    <?php 
-        echo "Hola mundo"
-    ?>
+        <?php
+        $nombre = "Zamira"; //Declaracion de variable,  de tipo (lo deduce, no es fuertemente tipado)
+        echo "Hola $nombre <br/>"; // Una variable entre comillas dobles es sustituida por su valor
+        echo 'Hola mundo <br/>'; // Puedo usar comillas simples para el eco
+        echo 'Hola $nombre <br>'; //Pero como haya variables se debe utilizar comilla doble o
+        echo 'Hola ', $nombre, '<br/>'; // Asi detecta la variable y se deba agregar el espacio manualmente porque si no, no lo detecta
+        echo "Hola ", strtoupper($nombre); //Se declara asi en mayuscula
+        ?>
     </h3>
 </body>
+
 </html>
