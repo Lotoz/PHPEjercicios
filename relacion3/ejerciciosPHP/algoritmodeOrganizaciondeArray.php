ed5af41ed0632b3893e20b27249059cc2f3802e0
Lotoz <144383551+Lotoz@users.noreply.github.com>
2025-10-31 18:40:41 +0100

    SuperIndex empezado

---
+++ relacion3/ejerciciosPHP/algoritmodeOrganizaciondeArray.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ascenso de burbujas</title>
+</head>
+<body>
+    <h1>Algoritmo de organizacion de arrays "Acenso de burbujas"</h1>
+    <?php
+        //Hacer un ejercicio de esto para practicas.
+        /**
+         * Se debe intercambiar dos valores
+         */
+        //Parametros por referencia
+        function swap(&$v1, &$v2){
+            //Ocurre en un ambito local
+            $intercambio = $v1;
+            $v1 = $v2;
+            $v2 = $intercambio;
+        }
+        //Hace lo de burbuja
+        function burbuja(&$array){
+            
+        }
+    ?>
+</body>
+</html>
